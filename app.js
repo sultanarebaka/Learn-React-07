24ae54e5aa0620e8f2b3976f0e6a8fcb733cdc54
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-24 20:33:21 +0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Header = () => {
+  return (
+    <>
+      <h1 style={{color: "red"}}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header />);
